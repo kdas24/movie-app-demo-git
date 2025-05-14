9a309116595c5be28588ec5ccd7007d98dd8b560
Koena Das <kdas@securdi.com>
2025-05-14 13:32:51 +0530

    testing modules

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import { act } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Search for movies/i);
   expect(linkElement).toBeInTheDocument();
 });
